a74104856e0e1f36e4b98e1f2e6f0e824242947c
abramov-pv <piotr.abram@gmail.com>
2024-11-14 17:51:50 +0500

    add descriptions for cache functions

--- cache.h
+++ cache.h
@@ -7,9 +7,19 @@
 extern "C" {
 #endif
 
+/**
+ * Frees passwd and group names cache.
+ */
 void pw_cache_free(void);
 
+/**
+ * Returns a cached if exist user name ortherwise adds a new mapping uid <-> username into cache.
+ */
 const char* get_user_name(uid_t uid, const char *username);
+
+/**
+ * Returns a cached if exist group name ortherwise adds a new mapping gid <-> group into cache.
+ */
 const char* get_group_name(gid_t gid, const char *groupname);
 
 #ifdef __cplusplus
